da96091ec3c6bc458704bb652e5fd0ff0afcc1a8
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-03-16 20:28:53 +0400

    Registration api created

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express"
 import mongoose from "mongoose"
 import { config } from "dotenv"
-import router from "./routes"
+import router from "./routes/index.js"
 config()
 
 const app = express()
 const PORT = 5001
-const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${DB_NAME}.cd6amzr.mongodb.net/?retryWrites=true&w=majority`
+const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.cd6amzr.mongodb.net/?retryWrites=true&w=majority`
 
 app.use(express.json())
 app.use('/api', router)
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,22 @@ import { check, validationResult } from "express-validator";
 const router = new Router();
 
 router.post('/registration', async (req, res) => {
+    try {
 
+        const { email, password } = req.body;
+        const candidate = await User.findOne({ email })
+
+        if (candidate) {
+            return res.status(400).json({ message: `User with email ${email} already exists ` })
+        }
+
+        const hashPassword = await bcrypt.hash(password, 15);
+        const user = new User({ email, password: hashPassword });
+        await user.save();
+        return res.json({ message: "User was created" })
+    } catch (err) {
+        res.send({ message: "Server error" })
+    }
 })
 
 export default router
