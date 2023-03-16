98417ff1761e50f13b000eb075c65eda30f8a8ba
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-03-16 20:21:00 +0400

    Registration

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import mongoose from "mongoose"
+import { config } from "dotenv"
+config()
+
+const app = express()
+const PORT = 5001
+const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${DB_NAME}.cd6amzr.mongodb.net/?retryWrites=true&w=majority`
+
+app.use(express.json())
+
+const start = async () => {
+    try {
+        mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+        app.listen(PORT, () => {
+            console.log("SERVER HAS STARTED ON ");
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start()
---
+++ models/User.js
@@ -0,0 +1,12 @@
+import { Schema, model, ObjectId } from "mongoose";
+
+const User = new Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    diskSpace: { type: Number, default: 1024 ** 3 * 10 },
+    usedSpace: { type: Number, default: 0 },
+    avatar: { type: String },
+    files: [{ type: ObjectId, ref: 'File' }]
+})
+
+export default model('User', User)
